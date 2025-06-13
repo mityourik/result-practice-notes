75e4ef6d068c28c561d1b2ec94387fbb10900825
Dmitry <mityourik@gmail.com>
2025-06-13 10:02:55 +0300

    add content to bd.json

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import styled from 'styled-components';
 
 const Div = styled.div`
-    background-color: #282c34;
     text-align: center;
 `;
 
 function App() {
     return (
         <Div>
-            <i className="fa fa-camera-retro"></i>
+            <i className="fa fa-calendar"></i>
             <h1>Welcome to My Styled App</h1>
         </Div>
     );
